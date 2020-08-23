d3438d1f55d2c6e10261b8dbbcbd5eda8499731d
Danila Radu <radu.danila@gmail.com>
2020-08-24 01:51:36 +0300

    new commit

--- tut_first_app/lib/main.dart
+++ tut_first_app/lib/main.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
+// void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Test App'));
+  void button1Click() {
+    print('Button1 clicked');
   }
 
+  void button2Click() {
+    print('Button2 clicked');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('Test text1'),
+            RaisedButton(
+              child: Text('Button 1'),
+              onPressed: button1Click,
+            ),
+            RaisedButton(
+              child: Text('Button 2'),
+              onPressed: () {
+                print('Button2 clicked');
+              },
+            ),
+            Text('Test text2'),
+          ],
+        ),
+      ),
+    );
+  }
 }
